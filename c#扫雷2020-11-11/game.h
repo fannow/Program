56e6d24ba8d0ebdccab60aae1e2f4b2bc83b1efd
fannow <185334690@qq.com>
2020-11-11 22:25:37 +0800

    简单扫雷代码编写

---
+++ c#扫雷2020-11-11/game.h
@@ -0,0 +1,19 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+
+#pragma warning(disable:4996)
+
+#define ROW 12
+#define COL 12
+#define NUMS 20
+
+void Menu();
+
+void Game();
+
+#endif
---
+++ c#扫雷2020-11-11/main.c
@@ -0,0 +1,24 @@
+#include "game.h"
+
+int main()
+{
+	int quit = 0;
+	int select = 0;
+	while (!quit){
+		Menu();
+		scanf("%d", &select);
+		switch (select){
+		case 1:
+			Game();
+			break;
+		case 2:
+			quit = 1;
+			break;
+		default:
+			printf("输入有误，重新输入!\n");
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
